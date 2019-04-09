84cd05edeb317e4747949816e465d5109e7937c9
dienigma <joshi.chinmay09@gmail.com>
2019-04-09 17:21:28 +0530

    made the static log in page.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
 
 const userRouter = require('./routes/user')
 app.use('/user',userRouter)
