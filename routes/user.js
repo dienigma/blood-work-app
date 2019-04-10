161e38026005dd812d146d8bbf3cd31a71c15b98
dienigma <joshi.chinmay09@gmail.com>
2019-04-10 10:52:09 +0530

    Worked on accordion

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,37 @@ const express = require('express')
 const router = express.Router()
 
 const credentials = {
-    name: "Chinmay",
+    name: "shahid",
     email: "chinmay@email.com",
     password: '123'
 }
 
-var data = {
-    name: "Mr. Chinmay Joshi",
-    title: "Diabetes Report",
-    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum integer. Et malesuada fames ac turpis egestas integer eget. Nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Et tortor consequat id porta nibh venenatis."
-
-}
+var data = [
+    {
+        id: 'headingOne',
+        collapseId:'collapseOne',
+        name: "Mr. Chinmay Joshi",
+        title: "Diabetes Report",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum integer. Et malesuada fames ac turpis egestas integer eget. Nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Et tortor consequat id porta nibh venenatis."
+    
+    },
+    {
+        id: 'headingTwo',
+        collapseId:'collapseTwo',
+        name: "Mr. Tom Smith",
+        title: "Ketone report",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum integer. Et malesuada fames ac turpis egestas integer eget. Nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Et tortor consequat id porta nibh venenatis."
+    
+    },
+    {
+        id: 'headingThree',
+        collapseId:'collapseThree',
+        name: "Mr. Mohammed Afzal",
+        title: "lipid Report",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur purus ut faucibus pulvinar elementum integer. Et malesuada fames ac turpis egestas integer eget. Nunc mi ipsum faucibus vitae aliquet nec. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Et tortor consequat id porta nibh venenatis."
+    
+    }
+]
 
 router.get('/login',(req,res) => {
     res.render('login', {credentials: credentials})
